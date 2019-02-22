841e467f590c389a6e0f97ab28ef0fa22525fe32
gialani <47328179+gialani@users.noreply.github.com>
2019-02-22 15:02:14 -0500

    Add files via upload

---
+++ account.php
@@ -0,0 +1,15 @@
+<?php
+// this database is used for testing
+$hostname = "localhost" ; 			
+$username = "testUser" ;
+$project  = "testdb" ;
+$password = "12345678" ;
+
+
+//this will be used for actual database for our project
+//$hostname = "localhost";
+//$username =  "dbadmin";
+//$project = "paepoq";
+//$password = "admin";
+
+?>
