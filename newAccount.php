6cb826afe4f72681c0c9702c9ca1f37e47659f07
Domnick96 <47873833+Domnick96@users.noreply.github.com>
2019-02-24 17:08:50 -0500

    Add files via upload

--- newAccount.php
+++ newAccount.php
@@ -18,7 +18,7 @@ width:500px;
   background: white;
   border-radius: 12px;
 }
-h1, h4{
+h1{
 text-align:center;
 }
 .button{
@@ -34,7 +34,7 @@ text-align:center;
         transform: translateY(5px);
         }
  /* Full-width input fields */
-input[type=text], input[type=password],input[type=number] {
+input[type=text], input[type=password],input[type=email] {
     width: 250px;
     padding: 12px 20px;
     margin: 0px 20;
@@ -45,38 +45,28 @@ input[type=text], input[type=password],input[type=number] {
 </style>
  
 </head>
-
-  <body>
-
-
-    <body bgcolor="#FF7F50">
+<body style="background-image:url('Pay.jpg');">
       
     <h1 style= color:red>New Account</h1>  
 
-
-<h4>Please Fill Out the Form Below</h4>
-
-
+<h1 style= color:red>Please Fill Out the Form Below</h1>
 
 	<form method="post" action="newAccount.php">
-
+<center>
    User Name:
-   <input type="text" name= "username"  placeholder = "Enter Your User Name" pattern="([A-Za-z]).{2,}" title="Name must be only letters and must be atleast 5 letters" autofocus required>
+   <input type="text" name= "username"  placeholder = "Enter your user name" pattern="([A-Za-z]).{2,}" title="Name must be only letters and must be atleast 5 letters" autofocus required>
    <br><br>
    
    Password:
-   <input placeholder="Enter New Password" type="password" name="password_1" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" required>
+   <input placeholder="Enter your password" type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" required>
  <br><br>
-  ReEnter Password:
-  <input placeholder="Confirm Password" type="password" name="password_2" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" required>
+   Email:
+  <input placeholder="Enter email address" type="Email" name="Email Address" pattern="[A-Za-z]{3}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 5 or more characters" required>
 <br><br>
-   Nick Name:
-  <input placeholder = "Your Nick Name" type="text" name="nickname" pattern="([A-Za-z]).{2,}" title="Name must be only letters and must be atleast 2 letters" required>
-  <br><br>
- Age:
-  <input type="number" name="age" placeholder = "Your Age">
-  <br> <br><br>
+  
   <button type="submit" name="reg_user" class="button">Register</button>
+ </center>
+ 
 </form>
 
 
