95b8144aa85447f0158f2caece7f54fd2de31c35
Domnick96 <47873833+Domnick96@users.noreply.github.com>
2019-02-28 19:18:28 -0500

    Update account.php

--- account.php
+++ account.php
@@ -6,6 +6,16 @@ $project  = "testdb" ;
 $password = "12345678" ;
 
 
+$db= mysqli_connect ($hostname,$username,$password,$project);
+if (mysqli_connect_errno())
+{
+echo "Failed To Connect To MySQL:" .mysqli_connect_error();
+exit();
+}
+print "Successfully Connected to MySQL.<br><br><br>";
+mysqli_select_db($db,$project);
+
+
 //this will be used for actual database for our project
 //$hostname = "localhost";
 //$username =  "dbadmin";
